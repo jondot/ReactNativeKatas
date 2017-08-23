f6f6d27ce74b1f5b373e0dd6e9471881715bf5cb
Viktor Nagy <viktor.nagy@balabit.com>
2017-08-23 15:38:19 +0200

    react-native linking

--- android/app/src/main/java/com/rnk/MainApplication.java
+++ android/app/src/main/java/com/rnk/MainApplication.java
@@ -3,6 +3,8 @@ package com.rnk;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.rnfs.RNFSPackage;
 import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNFSPackage(),
             new RNViewShotPackage()
       );
     }
