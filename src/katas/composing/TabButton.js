e72cef6474c296e6662be699810c2a98a10553a7
Doug Watkins <doug@fashionphile.com>
2018-04-25 14:20:16 -0600

    Finished tabBar kata.

--- src/katas/composing/TabButton.js
+++ src/katas/composing/TabButton.js
@@ -8,35 +8,93 @@ import ReactNative, {
 import Icon from 'react-native-vector-icons/Ionicons'
 import colors from '@/runner/colors'
 
-
-
-const TabButton = (props)=>{
+const TabButton = (props) => {
   return (
     <View style={styles.container}>
-      <Icon style={styles.text} name='ios-bulb-outline' size={120} />
-      <Text style={styles.text}>Tab Button</Text>
+      <View style={styles.tabBar} >
+        <View style={styles.tabButton} >
+        <View style={styles.textHeader}>
+            <Text style={[styles.text, {fontSize: 12}]}>Hidden Contacts</Text>
+          </View>
+          <View style={styles.iconContainer} >
+            <Icon style={styles.text} name='md-contacts' size={30} />
+          </View>
+        </View>
+        <View style={styles.tabButton} >
+          <View style={styles.textHeader}>
+            <Text style={[styles.text, {fontSize: 12}]}>{"Ditch\nThe Man"}</Text>
+          </View>
+          <View style={styles.iconContainer} >
+            <Icon style={styles.text} name='md-finger-print' size={30} />
+          </View>
+        </View>
+        <View style={styles.tabButton} >
+          <View style={styles.textHeader}>
+            <Text style={[styles.text, {fontSize: 12}]}>Chemical Burns</Text>
+          </View>
+          <View style={styles.iconContainer} >
+            <Icon style={styles.text} name='md-flask' size={30} />
+          </View>
+        </View>
+        <View style={styles.tabButton} >
+          <View style={styles.textHeader}>
+            <Text style={[styles.text, {fontSize: 12}]}>{"Feuer\nFrei"}</Text>
+          </View>
+          <View style={styles.iconContainer} >
+            <Icon style={styles.text} name='md-nuclear' size={30} />
+          </View>
+        </View>
+        <View style={[styles.tabButton, {borderRightWidth: 0}]} >
+          <View style={styles.textHeader}>
+            <Text style={[styles.text, {fontSize: 12}]}>{"Black\nMesa"}</Text>
+          </View>
+          <View style={styles.iconContainer} >
+            <Icon style={styles.text} name='md-aperture' size={30} />
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
     backgroundColor: colors[1],
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+
+  tabBar: {
+    backgroundColor: 'grey',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+
+  tabButton: {
+    borderRightWidth: 1,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+  },
+
+  textHeader: {
+    justifyContent: 'flex-start',
+    marginTop: 5,
+  },
+
+  iconContainer: {
+    justifyContent: 'flex-end',
+    margin: 10,
   },
 
   text:{
     color: 'white',
-    textAlign:'center'
+    textAlign:'center',
+    fontWeight: '100',
   },
 
 });
-
-
-
-
 
 TabButton.displayName = 'TabButton'
 import TabButtonTest from './TabButton.test'
--- src/katas/composing/TabButton.test.js
+++ src/katas/composing/TabButton.test.js
@@ -9,90 +9,55 @@ import ReactNative, {
 import Icon from 'react-native-vector-icons/Ionicons'
 import colors from '@/runner/colors'
 
-
-
 /** Composing
 *
-* Build a tab button user interface
+* Build a tabBar type user interface
+* Cascading styles will be important
+* Also think in terms on containers,
+*  this includes containers inside of containers that could be inside
+*  of other containers ... and so on and so forth
 *
+* Lastly, because this is a Kata, these will not be actual buttons, just
+*  views to test your layout chops.
 */
 
-const TabButton= (props)=>{
+const TabButton = (props) => {
   return (
     <View style={styles.container}>
-      <View style={styles.tabBar} >
-        <View style={styles.tabButton} >
-          <View style={styles.textHeader}>
-            <Text style={[styles.text, {fontSize: 12}]}>Contacts</Text>
-          </View>
-          <View style={styles.iconContainer} >
-            <Icon style={styles.text} name='md-contacts' size={30} />
-          </View>
-        </View>
-        <View style={styles.tabButton} >
-          <Text style={[styles.text, {fontSize: 12}]}>The Man</Text>
-          <Icon style={styles.text} name='md-finger-print' size={30} />
-        </View>
-        <View style={styles.tabButton} >
-          <Text style={[styles.text, {fontSize: 12}]}>Chem Burn</Text>
-          <Icon style={styles.text} name='md-flask' size={30} />
-        </View>
-        <View style={styles.tabButton} >
-          <Text style={[styles.text, {fontSize: 12}]}>{"Don't Touch"}</Text>
-          <Icon style={styles.text} name='md-nuclear' size={30} />
-        </View>
-        <View style={[styles.tabButton, {borderRightWidth: 0}]} >
-          <Text style={[styles.text, {fontSize: 12}]}>Apperture Science</Text>
-          <Icon style={styles.text} name='md-aperture' size={30} />
-        </View>
+      <View style={styles.tabBar}>
+        <Text style={styles.text}>Hidden Contacts</Text>
+        <Icon style={styles.text} name='md-contacts' size={30} />
+        <Text style={styles.text}>{"Ditch\nThe Man"}</Text>
+        <Icon style={styles.text} name='md-finger-print' size={30} />
+        <Text style={styles.text}>Chemical Burns</Text>
+        <Icon style={styles.text} name='md-flask' size={30} />
+        <Text style={styles.text}>{"Feuer\nFrei"}</Text>
+        <Icon style={styles.text} name='md-nuclear' size={30} />
+        <Text style={styles.text}>{"Black\nMesa"}</Text>
+        <Icon style={styles.text} name='md-aperture' size={30} />
       </View>
     </View>
   )
 }
 
-
 // TODO: There are no borders on the right of the right most tab button
-//
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors[1],
     flex: 1,
-    justifyContent: 'flex-end',
   },
 
   tabBar: {
     backgroundColor: 'grey',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-  },
-
-  tabButton: {
-    borderRightWidth: 1,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-  },
-
-  textHeader: {
-    justifyContent: 'flex-start',
-  },
-
-  iconContainer: {
-    justifyContent: 'flex-end',
   },
 
   text:{
     color: 'white',
     textAlign:'center',
-    fontWeight: '100',
   },
 
 });
-
-
-
-
 
 TabButton.displayName = 'TabButton'
 export default TabButton
